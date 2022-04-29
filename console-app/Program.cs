cbd615b28936733b585345d9d6653aca517d97a8
Douglas Craici <douglas.craici@clouddata.com.vc>
2022-04-29 15:08:50 -0300

    connectionString removed from program.cs

--- console-app/Program.cs
+++ console-app/Program.cs
@@ -1,30 +1,35 @@
 ﻿using System;
+using System.Configuration;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace omie_queue
 {
     public class Program
     {
-
         static void Main(string[] args)
         {
             Fila fila = new Fila();
             FilaAsync filaAsync = new FilaAsync();
             Mensagem mensagem = new Mensagem();
-            var connectionString = "DefaultEndpointsProtocol=https;AccountName=testedc1994;AccountKey=zAdSF6pKzjIwSNct0dH5YQwmJdyh9P0QpvIk4b21eazB9qEofy82IEkBRRH+6C1dZARrFCLu/P+aTOszXWHdvw==;EndpointSuffix=core.windows.net";
-            var nomeFila = "itens";
-            var mensagemParaAdd = "Mensagem 03";
+
+            var builder = new ConfigurationBuilder()
+                   .AddJsonFile("appsettings.json");
+            var config = builder.Build();
+            var connectionString = config["AzureQueue"];
+            var nomeFila = config["QueueName"];
+            var mensagemParaAdd = "Mensagem 01";
 
             // fila.CriarCliente(connectionString, nomeFila);     
             // fila.CriarFila(connectionString, nomeFila);
             // fila.ExcluirFila(connectionString, nomeFila);
-            // mensagem.AdicionarMensagem(connectionString, nomeFila, mensagemParaAdd);
+            mensagem.AdicionarMensagem(connectionString, nomeFila, mensagemParaAdd);
             // mensagem.EspiarMensagem(connectionString, nomeFila);
             // mensagem.EditarMensagem(connectionString, nomeFila);
             // mensagem.ApagarMensagem(connectionString, nomeFila);
             // mensagem.ApagarMensagem2(connectionString, nomeFila);
             // mensagem.VerificarQuantidadeMsgEmFila(connectionString, nomeFila);
-            
+
 
             // filaAsync.FilaAsyncAwait(connectionString, nomeFila);
 
